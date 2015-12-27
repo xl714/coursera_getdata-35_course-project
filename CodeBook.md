091534e2876d34bac67fd1dab915225193f3fbc4
unknown <XLanglois@PPC-P15679.reseauprod.adm>
2015-12-27 23:25:01 +0100

    init

---
+++ CodeBook.md
@@ -0,0 +1,107 @@
+Code Book
+=========
+
+Raw data
+----------
+
+The raw datas were found here : https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+
+Merging and cleaning steps in run_analysis.R
+--------------------
+
+Following transformations were performed by `run_analysis.R` script:
+
+1. Load separated datas
+2. Merges the training and the test sets to create one data set
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names
+5. Extracts only the measurements on the mean and standard deviation for each measurement
+6. From the data set in step before, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+7. Store tidy output to a text file
+
+tidy_data.txt
+-------------
+
+The output clean datas is stored in the tidy_data.txt file. 
+This is a text file with space-separated values which you can load with the following R code
+
+    data <- read.table(file_path, header = TRUE)
+    View(data)
+
+
+Fields in output data set:
+
+  Id variables :
+  
+    subject 
+    activity
+    
+  Measurments variables:
+  
+    tBodyAcc.mean...X
+    tBodyAcc.mean...Y
+    tBodyAcc.mean...Z
+    tBodyAcc.std...X
+    tBodyAcc.std...Y
+    tBodyAcc.std...Z
+    tGravityAcc.mean...X
+    tGravityAcc.mean...Y
+    tGravityAcc.mean...Z
+    tGravityAcc.std...X
+    tGravityAcc.std...Y
+    tGravityAcc.std...Z
+    tBodyAccJerk.mean...X
+    tBodyAccJerk.mean...Y
+    tBodyAccJerk.mean...Z
+    tBodyAccJerk.std...X
+    tBodyAccJerk.std...Y
+    tBodyAccJerk.std...Z
+    tBodyGyro.mean...X
+    tBodyGyro.mean...Y
+    tBodyGyro.mean...Z
+    tBodyGyro.std...X
+    tBodyGyro.std...Y
+    tBodyGyro.std...Z
+    tBodyGyroJerk.mean...X
+    tBodyGyroJerk.mean...Y
+    tBodyGyroJerk.mean...Z
+    tBodyGyroJerk.std...X
+    tBodyGyroJerk.std...Y
+    tBodyGyroJerk.std...Z
+    tBodyAccMag.mean..
+    tBodyAccMag.std..
+    tGravityAccMag.mean..
+    tGravityAccMag.std..
+    tBodyAccJerkMag.mean..
+    tBodyAccJerkMag.std..
+    tBodyGyroMag.mean..
+    tBodyGyroMag.std..
+    tBodyGyroJerkMag.mean..
+    tBodyGyroJerkMag.std..
+    fBodyAcc.mean...X
+    fBodyAcc.mean...Y
+    fBodyAcc.mean...Z
+    fBodyAcc.std...X
+    fBodyAcc.std...Y
+    fBodyAcc.std...Z
+    fBodyAccJerk.mean...X
+    fBodyAccJerk.mean...Y
+    fBodyAccJerk.mean...Z
+    fBodyAccJerk.std...X
+    fBodyAccJerk.std...Y
+    fBodyAccJerk.std...Z
+    fBodyGyro.mean...X
+    fBodyGyro.mean...Y
+    fBodyGyro.mean...Z
+    fBodyGyro.std...X
+    fBodyGyro.std...Y
+    fBodyGyro.std...Z
+    fBodyAccMag.mean..
+    fBodyAccMag.std..
+    fBodyBodyAccJerkMag.mean..
+    fBodyBodyAccJerkMag.std..
+    fBodyBodyGyroMag.mean..
+    fBodyBodyGyroMag.std..
+    fBodyBodyGyroJerkMag.mean..
+    fBodyBodyGyroJerkMag.std..
---
+++ run_analysis.R
@@ -0,0 +1,121 @@
+
+
+# 1) a tidy data set as described below, 
+# 2) a link to a Github repository with your script for performing the analysis, and 
+# 3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. 
+#    You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.  
+# 
+# One of the most exciting areas in all of data science right now is wearable computing - see for example this article : 
+#    http://www.insideactivitytracking.com/data-science-activity-tracking-and-the-battle-for-the-worlds-top-sports-brand/ 
+# Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. 
+# The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. 
+# A full description is available at the site where the data was obtained: 
+#     http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+# Here are the data for the project: 
+#     https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+# 
+# You should create one R script called run_analysis.R that does the following. 
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# 
+# help here : https://thoughtfulbloke.wordpress.com/2015/09/09/getting-and-cleaning-the-assignment/
+
+# clearn env
+rm(list = ls())
+setwd("~/r-work/coursera_getdata-35_course-project")
+
+# load for later use of filter and select
+library(dplyr)
+
+# Load separated datas
+x_train <- read.table('UCI_HAR_Dataset/train/X_train.txt')
+y_train <- read.table('UCI_HAR_Dataset/train/y_train.txt')
+subject_train <- read.table('UCI_HAR_Dataset/train/subject_train.txt')
+x_test <- read.table('UCI_HAR_Dataset/test/X_test.txt')
+y_test <- read.table('UCI_HAR_Dataset/test/y_test.txt')
+subject_test <- read.table('UCI_HAR_Dataset/test/subject_test.txt')
+
+# get features.txt to set the colums names later 
+features <- read.table('UCI_HAR_Dataset/features.txt', sep = " ", stringsAsFactors = FALSE) # stringsAsFactors for merge later with col names
+
+# get activity labels
+activityLabels <- read.table('UCI_HAR_Dataset/activity_labels.txt', sep = " ")
+
+# keep only features name and clean them
+features <- features$V2
+#features <- tolower(features)
+#features <- gsub("\\(\\)","",features)
+#features <- gsub("\\(|\\)|,","-",features)
+features <- make.names(names=features, unique=TRUE, allow_ = TRUE)
+
+# keep only activity labels and clean them
+activityLabels <- activityLabels$V2
+
+# create a dataset for test and train datas
+train <- data.frame(subject_train, y_train, x_train)
+test <- data.frame(subject_test, y_test, x_test)
+
+# remove variables unused anymore
+rm(x_train, y_train, subject_train, x_test, y_test, subject_test)
+
+# before merge : add a column "datafrom" to keep track of where the data comes from
+train <- tbl_df(train)
+test <- tbl_df(test)
+train <- mutate(train, datafrom = "train")
+test <- mutate(test, datafrom = "test")
+
+
+#################################################################################
+######## 1. Merges the training and the test sets to create one data set.######## 
+
+df <- merge(train, test, all=TRUE)
+df <- tbl_df(df)
+# > dim(df)
+# [1] 10299   564
+# > names(df)
+
+
+####################################################################################################
+######## 3. Uses descriptive activity names to name the activities in the data set ######## 
+
+df <- mutate(df, V1.1 = activityLabels[V1.1])
+
+
+#######################################################################################
+######## 4. Appropriately labels the data set with descriptive variable names. ######## 
+
+cols <- c("subject", "activity", features, "datafrom")
+
+colnames(df) <- cols
+
+
+############################################################################################################
+######## 2. Extracts only the measurements on the mean and standard deviation for each measurement. ########  
+
+df <- select(df, subject, activity, datafrom, contains(".mean."), contains(".std."))
+
+
+##################################################################################################
+######## 5. From the data set in step 4, creates a second, independent tidy data set with ######## 
+######## the average of each variable for each activity and each subject. ########################
+
+library("reshape2")
+
+# prepare measure.vars for melting 
+dfColNames <- names(df)
+
+dfMelt <- melt(df, id.vars = c("subject", "activity", "datafrom"), measure.vars = dfColNames[4:length(dfColNames)])
+
+tidy_dataset <- dcast(dfMelt, subject + activity ~ variable, mean)
+
+write.table(tidy_dataset, "tidy_dataset.txt", row.names = FALSE, quote = FALSE)
+
+# to read the data set use :
+# data <- read.table(file_path, header = TRUE)
+# View(data)
+
+
+
