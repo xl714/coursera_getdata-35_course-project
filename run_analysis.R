bcf1e907729cb70672e8df5fabb3962fa2f3797a
unknown <XLanglois@PPC-P15679.reseauprod.adm>
2015-12-27 23:37:59 +0100

    add readme text

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,4 @@
 
-
-# 1) a tidy data set as described below, 
-# 2) a link to a Github repository with your script for performing the analysis, and 
-# 3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. 
-#    You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.  
-# 
 # One of the most exciting areas in all of data science right now is wearable computing - see for example this article : 
 #    http://www.insideactivitytracking.com/data-science-activity-tracking-and-the-battle-for-the-worlds-top-sports-brand/ 
 # Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. 
